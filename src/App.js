c91e06ed65398494debdff744270a3a2fdda6605
Moien <137896500+moienheydari@users.noreply.github.com>
2024-03-18 02:20:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+        <p>
+          Hello World!
+        </p>
+    </div>
+  );
+}
