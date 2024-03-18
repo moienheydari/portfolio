9606e96824d9e111303be1fcdaa9ba640dfb380f
Moien <137896500+moienheydari@users.noreply.github.com>
2024-03-18 02:35:29 +0100

    setting up initial css and background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './css/App.css';
 import React from 'react';
+import backgroundImage from './Assests/background.jpg';
 
 export default function App() {
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh'
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={backgroundStyle}>
+
         <p>
           Hello World!
         </p>
